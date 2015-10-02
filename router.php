3828e565d8c999b41e36aba7f96f5fbaed4f6bde
Yvo Brevoort <yvo@muze.nl>
2015-10-02 23:00:25 +0200

    added basic 404 handler (router.php) to allow multiple page templates added page template dialog (still needs a way to be configged) updated path in image-browse (still needs proper config)

---
+++ router.php
@@ -0,0 +1,18 @@
+<?php
+	$json = file_get_contents("http://yvo.muze.nl/ariadne/loader.php/system/users/yvo/simply-edit-data/get");
+	$data = json_decode($json, true);
+
+	if (isset($data[$_SERVER["REQUEST_URI"]])) {
+		header("HTTP/1.1 200 OK");
+
+		$template = "index.html";
+		$pageTemplate = $data[$_SERVER["REQUEST_URI"]]['data-vedor-page-template']['content'];
+		if (file_exists($pageTemplate) && preg_match("/\.html$/", $pageTemplate)) {
+			$template = $pageTemplate;
+		}
+		include($template);
+	} else {
+		header("HTTP/1.1 404 Not Found");
+		echo "<h1>Page not found (error: 404)</h1>";
+	}
+?>
