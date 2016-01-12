2485b85b04e33fa04cbb1a2728b36855aa7f520c
Robbert Müller <spam.me@grols.ch>
2016-01-12 20:19:44 +0100

    some runner code

--- tests/data-display/run-tests.js
+++ tests/data-display/run-tests.js
@@ -1,5 +1,65 @@
 var page = require('webpage').create();
 var url = 'http://localhost/tests/data-display/';
+
+function setupCallbacks() {
+	window.document.addEventListener('DOMContentLoaded', function() {
+		// Setup Error Handling
+		var qunit_error = window.onerror;
+
+		window.onerror = function ( error, filePath, linerNr ) {
+			qunit_error(error, filePath, linerNr);
+			if (typeof window.callPhantom === 'function') {
+				window.callPhantom({
+					'name': 'Window.error',
+					'error': error,
+					'filePath': filePath,
+					'linerNr': linerNr
+				});
+			}
+		};
+
+		var callback = function(name) {
+			return function(details) {
+				if (typeof window.callPhantom === 'function') {
+					window.callPhantom({
+						'name': 'QUnit.' + name,
+						'details': details
+					});
+				}
+			};
+		};
+
+		var i, callbacks = [
+			'begin', 'done', 'log',
+			'moduleStart', 'moduleDone',
+			'testStart', 'testDone'
+		];
+		for (i=0; i<callbacks.length;i+=1) {
+			QUnit[callbacks[i]](callback(callbacks[i]));
+		}
+
+	}, false);
+}
+
+
+// Route `console.log()` calls from within the Page context to the main Phantom context (i.e. current `this`)
+page.onConsoleMessage = function(msg) {
+	console.log(msg);
+};
+
+page.onInitialized = function() {
+	page.evaluate(setupCallbacks);
+};
+
+page.onError = function(msg, trace) {
+	console.error(color('error message', msg));
+	trace.forEach(function(item) {
+		console.error(color('error stack', '  ' + item.file + ':' + item.line));
+	});
+	console.error('');
+	phantom.exit(1);
+};
+
 page.open(url, function (status) {
 	//Page is loaded!
 	var testsDone = function() {
