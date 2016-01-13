488688d651d05e4c2ed5d015c774d53062928174
Robbert Müller <muller@muze.nl>
2016-01-13 11:59:45 +0100

    add missing js/ in loader

--- loader/simply-edit.js
+++ loader/simply-edit.js
@@ -1,6 +1,6 @@
 var scriptTag = document.querySelector("[data-api-key]");
 var newTag = document.createElement("SCRIPT");
 newTag.id  = 'SimplyEditScript';
-newTag.src = scriptTag.src.replace("simply-edit.js", scriptTag.getAttribute("data-api-key") + "/simply-edit.js");
+newTag.src = scriptTag.src.replace("simply-edit.js", scriptTag.getAttribute("data-api-key") + "/js/simply-edit.js");
 document.head.appendChild(newTag);
 
