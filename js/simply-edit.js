eedc505e418dcffdcce03573d1768a24b64a775d
Yvo Brevoort <yvo@muze.nl>
2016-02-17 14:15:53 +0100

    remove body opacity calls added data-simply-edit attribute to body when editmode is started; fixes #24

--- js/simply-edit.js
+++ js/simply-edit.js
@@ -280,10 +280,7 @@
 				} 
 			},
 			load : function() {
-				document.body.style.opacity = 0;
-
 				editor.storage.load(function(data) {
-					document.body.style.opacity = 1;
 					localStorage.data = data;
 					editor.currentData = JSON.parse(data);
 					editor.data.apply(editor.currentData, document);
@@ -896,6 +893,8 @@
 						return message;
 					}
 				};
+
+				document.body.setAttribute("data-simply-edit", true);
 
 				document.body.onbeforeunload = handleBeforeUnload; // Must do it like this, not with addEventListener;
 				
