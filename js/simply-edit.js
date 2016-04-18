5ce0d1f77bf43a1af954666f268dd2ab38aa61ab
Yvo Brevoort <yvo@muze.nl>
2016-04-18 11:04:38 +0200

    - fix for logout - fix for firefox mac, adding list items not working with mouse click

--- js/simply-edit.js
+++ js/simply-edit.js
@@ -1537,7 +1537,7 @@
 				http.setRequestHeader("Authorization", "Basic ABCDEF");
 
 				http.onreadystatechange = function() {//Call a function when the state changes.
-					if(http.readyState == 4 && http.status == 401) {
+					if(http.readyState == 4 && ((http.status > 399) && (http.status < 500)) ) {
 						callback();
 					}
 				};
